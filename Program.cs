f162978e28c867371da643db642d2c7926f32fd2
Carolina Viana <carollinasilva057@gmail.com>
2025-09-17 11:00:15 -0300

    Update Program.cs for Render deployment

--- Program.cs
+++ Program.cs
@@ -2,6 +2,15 @@ using ParkingSystemApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowGitHubPages",
+        policy => policy
+            .WithOrigins("https://carolina-sv.github.io")
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,5 +27,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+app.UseCors("AllowGitHubPages");
+
 app.MapControllers();
-app.Run();
+
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+app.Run($"http://0.0.0.0:{port}");
